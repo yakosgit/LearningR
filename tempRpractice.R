e8f1aa6aff50349f01591dbca32407aef1e1df3b
yakosgit <73800545+yakosgit@users.noreply.github.com>
2022-08-30 14:49:30 -0400

    some scribble

--- tempRpractice.R
+++ tempRpractice.R
@@ -1,2 +1,23 @@
 x <- c(1:10)
 x
+y = "hello world"
+y
+
+l <- TRUE
+i <- 123L
+n <- 1.23
+c <- "ABC 123"
+d <- as.Date("2001-02-03")
+
+f <- function(x) {
+  x <- x*2
+  return(x)
+}
+
+f(2)
+
+s <- 1:6
+s
+l <- list(l, i, n, c, d)
+l
+install.packages("dplyr")
